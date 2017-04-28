890e4f63ec4d76f553f305255b98845c222c2d67
Daniela Popova <d.i.popova@abv.bg>
2017-04-28 14:13:48 +0300

    whateveeer

--- petstore/main.js
+++ petstore/main.js
@@ -1,7 +1,7 @@
  import {getTemplate} from './scripts/helpers/template.js';
  import { data } from 'data';
  import { router } from 'router';
- import { getAllData } from './firebaseDB.js';
+ 
 
  
  router.init();
