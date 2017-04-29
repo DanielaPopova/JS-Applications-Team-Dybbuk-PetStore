ae07472b9116503fc2b4a7191e9cc14da354dfb2
velev1 <velev_bg@mail.bg>
2017-04-29 06:04:53 +0300

    ADD getTemplateAjax function.

--- petstore/main.js
+++ petstore/main.js
@@ -1,27 +1,60 @@
- import {getTemplate} from './scripts/helpers/template.js';
- import { data } from 'data';
- import { router } from 'router';
- import * as db from 'db';
- 
- // TESTING methods to extract data from firebase
-db.filterCatFoodByAmount(1.5).then(function(filteredFood){
-    if(filteredFood.length === 0){
-		console.log('no results');
-	}
+import { getTemplate } from './scripts/helpers/template.js';
+import { data } from 'data';
+import { router } from 'router';
+import * as db from 'db';
+import handlebars from 'handlebars';
+import $ from 'jquery';
 
-	filteredFood.forEach(function(food){
-		let img = document.createElement('img');
-		img.style.width = '200px';
-		img.style.height = '200px';
-		img.src = food.imageURL;	
-		let div = document.getElementById('main-content-container');
-		div.appendChild(img);
-		console.log(food.name);		
-	});
+// TESTING methods to extract data from firebase
+// db.filterCatFoodByAmount(1.5).then(function(filteredFood){
+//     if(filteredFood.length === 0){
+// 		console.log('no results');
+// 	}
+
+// 	filteredFood.forEach(function(food){
+// 		let img = document.createElement('img');
+// 		img.style.width = '200px';
+// 		img.style.height = '200px';
+// 		img.src = food.imageURL;	
+// 		let div = document.getElementById('main-content-container');
+// 		div.appendChild(img);
+// 		console.log(food.name);		
+// 	});
+// });
+
+//router.init();
+db.getAllCatFood().then(function (filteredFood) {
+   // filteredFood.forEach(function (food) {
+        // let img = document.createElement('img');
+        // let p = document.createElement('p');
+        // p.innerHTML = food.name;
+        // img.style.width = '200px';
+        // img.style.height = '200px';
+        // img.src = food.imageURL;
+        // let div = document.getElementById('main-content-container');
+        // div.appendChild(img);
+        // div.appendChild(p);
+
+         console.log(filteredFood);
+        getTemplateAjax('templates/cat-food.handlebars');
+   // });
 });
- 
-router.init();
-getTemplate();
 
- let test = data.testFunction();
+function getTemplateAjax(path) {
+    var source;
+    var template;
+
+    $.ajax({
+        url: path, //ex. js/templates/mytemplate.handlebars
+        cache: true,
+        success: function (data) {
+            source = data;
+            console.log(source);
+            template = handlebars.compile(source);
+            $('#main-content-container').html(template);
+        }
+    });
+}
+
+let test = data.testFunction();
 console.log(' test ' + test);
