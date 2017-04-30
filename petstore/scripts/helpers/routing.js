07a86039efecc411185cca9f78a4afc0bfea4023
Daniela Popova <d.i.popova@abv.bg>
2017-04-30 21:46:22 +0300

    fixed merge

--- petstore/scripts/helpers/routing.js
+++ petstore/scripts/helpers/routing.js
@@ -1,11 +1,7 @@
 import { productsController } from 'products-controller';
 import { homeController } from 'home-controller';
-<<<<<<< HEAD
 import { userController } from 'user-controller';
-=======
-import { userControler } from 'user-controller';
 import { cartController } from 'cart-controller';
->>>>>>> 381a49ebd7cc04214703abc264b049950cd607b6
 
 let router = (() => {
     let router;
