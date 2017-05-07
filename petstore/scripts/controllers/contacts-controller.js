46ec9548a7a23d436c4b4678390e952f3e5b0069
Daniela Popova <d.i.popova@abv.bg>
2017-05-07 17:54:41 +0300

    added contacts /without validation

--- petstore/scripts/controllers/contacts-controller.js
+++ petstore/scripts/controllers/contacts-controller.js
@@ -3,9 +3,26 @@ import $ from 'jquery';
 import { getTemplate } from 'templates';
 
 class ContactsController {
+
+    getAcademyLocation() {
+        let location = new Promise(function(resolve, reject){
+            let latitude = 42.650880,
+                longitude = 23.379507,
+                source = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}
+                          &zoom=17
+                          &size=400x400
+                          &markers=color:red%7Clabel:HHH%7C${latitude},${longitude}
+                          &sensor=false`;
+            resolve(source);
+        }).then(function(source) {
+            $('#map-image').attr("src", source);
+        }); 
+    }
+
     loadContacts() {
         getTemplate('contacts').then((template) => {
-            $('#main-content-container').html(template());			
+            $('#main-content-container').html(template());
+            this.getAcademyLocation();			
         })
     }
 }
